0cc46758d952a606d506384b5f63e8285a20562b
张浩 <zhanghao.0929@qq.com>
2017-07-17 16:42:46 +0800

    add commonsInterceptor

---
+++ src/main/java/com/example/interceptor/CommonsInterceptor.java
@@ -0,0 +1,25 @@
+package com.example.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by zhang on 2017/7/17.
+ */
+public class CommonsInterceptor implements HandlerInterceptor {
+
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
