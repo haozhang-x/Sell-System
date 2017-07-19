09f4301b87754f37e119937daf63c9c50a048d45
zhang <zhanghao.0929@qq.com>
2017-07-19 17:39:22 +0800

    some changes

--- src/main/java/com/example/web/controller/api/LoginApiController.java
+++ src/main/java/com/example/web/controller/api/LoginApiController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import javax.servlet.http.HttpSession;
 
